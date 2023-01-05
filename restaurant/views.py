e4e0d45cea041547d66f8dc09e157c1ea17c6798
Joshua <cutejosh2@gmail.com>
2023-01-05 21:40:17 +0100

    restaurant crud views

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,16 @@
 from rest_framework import generics
-from .serializers import MenuItemSerializer, OrdersSerializer, OrderItemsSerializer
-from .models import MenuItem, Orders, OrderItems
+from .serializers import MenuItemSerializer, OrdersSerializer, OrderItemsSerializer, RestaurantSerializer
+from .models import MenuItem, Orders, OrderItems, Restaurant
+
+class RestaurantListView(generics.ListAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+
+class RestaurantDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    lookup_field = 'pk'
 
 class MenuListView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
