b17713051ca8a88042efc9939867d0c86aba3709
Joshua <cutejosh2@gmail.com>
2023-01-09 23:25:20 +0100

    removed password field from customer

--- users/models.py
+++ users/models.py
@@ -6,7 +6,6 @@ class Customer(models.Model):
 	name = models.CharField(max_length=255)
 	email = models.EmailField()
 	phone = models.CharField(max_length=255)
-	password = models.CharField(max_length=255)
 
 	def __str__(self):
 		return f"{self.name} ({self.email})"
