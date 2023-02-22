31910a359d25d55946300c5e8334dca7c5e2aab9
Joshua Hassan <cutejosh2@gmail.com>
2023-02-22 06:35:31 +0100

    we already have api/ in root urls config

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,8 +7,8 @@ from . import views
 urlpatterns = [
 	path('register/', views.registration_view, name='registr_view'),
 	path('token/', views.login_view, name='login_view'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    	path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 	path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-    path('api/change-password/', views.ChangePasswordView.as_view(), name='change-password'),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    	path('change-password/', views.ChangePasswordView.as_view(), name='change-password'),
+    	path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
